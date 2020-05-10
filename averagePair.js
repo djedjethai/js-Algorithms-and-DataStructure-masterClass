3426b58b91bd2a16e051a4543abbb4730f174a6d
djedjethai <djedje.thai.ok@gmail.com>
2020-05-10 20:14:33 +0700

    average pair looks good

--- averagePair.js
+++ averagePair.js
@@ -1,35 +1,37 @@
 function averagePair(arr, b){
-   // multiplie nbr par 2
-   const somme =  b * 2;
-   let str = 0;
-   let end = somme; 
-   
-   // sort arr
-   arr.sort(function(a, b) {
-    return a - b;
-    })
-
-   // cut number sup a somme(via loop)< length
-   for(let i = 0; i < somme; i ++) {
-
-    let fnd = somme - arr[str]
+    // multiplie nbr par 2
+    const somme =  b * 2;
     
-    if( fnd > arr[end] ) {
-        str += 1;
-    } 
-
-    if( fnd < arr[end]) {
-        str -= 1;
+    // cut too big numbers
+    var str = 0;
+    var end = arr.findIndex(elm => elm > somme )
+    if (end === -1) {
+        end = (arr.length) - 1;
     }
-
-    if( fnd === arr[end]) {
-        return true;
+ 
+    // sort arr
+    arr.sort(function(a, b) {
+     return a - b;
+     })
+ 
+    // move the cursor 
+    for(let i = 0; i < end; i ++) {
+ 
+     let fnd = somme - arr[str];
+     
+     if( fnd > arr[end] ) {
+         str += 1;
+     } 
+ 
+     if( fnd < arr[end]) {
+         end -= 1;
+     }
+ 
+     if( fnd === arr[end]) {
+         return true;
+     }
     }
-
-   }
-
-   return false
-
-}
-
-cconsole.log(averagePair([2,5,7,9,11], 8))
+    return false
+ }
+ 
+ console.log(averagePair([2,5,7,10,11, 23, 47], 8))
