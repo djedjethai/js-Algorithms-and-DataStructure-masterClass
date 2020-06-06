82294be7c184410112148d99ca1da7b3c382f694
djedjethai <djedje.thai.ok@gmail.com>
2020-06-06 20:34:36 +0700

    fingLongestSubString almost done

--- findLongestSubstring.js
+++ findLongestSubstring.js
@@ -3,6 +3,7 @@ function findLongestSubstring(str){
     let left = 0;
     let right = 1;
     let done = false;
+    let longStr = Object.keys(str).length;
     
     let obj = {};
     
@@ -22,13 +23,9 @@ function findLongestSubstring(str){
     
     obj[str[left]] = 1;
     
-    console.log('left start');
-    console.log(left);
-    console.log('right start');
-    console.log(right);
     
-    // loop, a affiner................
-    for (let y = 0; y < Object.keys(str).length - (left + 1); y++) {
+    let run = false;
+    while (run != true) {
         // parse char into obj
         if (!obj[str[right]]) {
             console.log('right');    
@@ -40,21 +37,25 @@ function findLongestSubstring(str){
         // if (obj[str[right]]) decale right until match (left ref is the str obj) && add char to the obj[char] = 1;
         } else if (str[left] != str[right]) {
             
-            let i = left + 1;
+            let i = left;
             let done = false;
             console.log('tamere la pute')
             console.log(str[right]);
     
-            while (done = false) {
-                if (str[i] === str[right]) {
-                    for (left; left = i; left++) {
-                        console.log('first -');
-                        obj[str[left]] = 0;
-                        cont--;
-                    }
+            while (done == false) {
+                if (str[left] === str[right]) {
+                    console.log('ds while 1');
+                    obj[str[left]] = 0;
+                    left++;
+                    cont--;
+                    if (left === right)
+                        right++;
                     done = true;
                 } else {
-                    i++;
+                    console.log('ds while 2');
+                    obj[str[left]] = 0;
+                    left++;
+                    cont--;
                 }
             }
            
@@ -68,6 +69,10 @@ function findLongestSubstring(str){
             obj[str[left]] = 0;
             cont--;
             left++;
+        }
+
+        if (longStr === right) {
+            run = true;
         }
     
     }
