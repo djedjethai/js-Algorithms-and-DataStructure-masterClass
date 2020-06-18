960d8361bfd2f06f3a68f6c78a5913b7dfbb8f25
djedjethai <djedje.thai.ok@gmail.com>
2020-06-18 20:14:36 +0700

    factorial recursive

--- recursion/sumRange.js
+++ recursion/sumRange.js
@@ -12,6 +12,10 @@ function sumRange(num) {
     if (num === 1) return 1;
     return num + sumRange(num - 1);
 }
+console.log(sumRange(4));
 
-
-sumRange(4);
+    // final return == 10
+    // 4 + sumRange(3) => 4+6
+    //         3 + sumRange(2) => 3+3
+    //                 2 + sumRange(1) => 2+1 
+    //                         return 1
