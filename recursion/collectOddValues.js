29bda5bf7a04945878dd1bb3d99680de728c89b7
djedjethai <djedje.thai.ok@gmail.com>
2020-06-19 20:46:51 +0700

    collectOddValues

---
+++ recursion/collectOddValues.js
@@ -0,0 +1,22 @@
+function collectOddValues(arr) {
+
+    let results = [];
+    function helper(helperArr) {
+        if (helperArr.length === 0) {
+            return;
+        }
+
+        if (helperArr[0] % 2 !== 0) {
+            results.push(helperArr[0]);   
+        }
+        helper(helperArr.slice(1));
+    }
+
+    helper(arr);
+
+    return results
+}
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(collectOddValues(arr));
