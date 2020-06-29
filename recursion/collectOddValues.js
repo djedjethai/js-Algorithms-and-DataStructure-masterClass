edf6e50f17131d5a2beb23ea7f49f4a77022129f
djedjethai <djedje.thai.ok@gmail.com>
2020-06-29 19:54:17 +0700

    rec power done

--- recursion/collectOddValues.js
+++ recursion/collectOddValues.js
@@ -1,4 +1,4 @@
-function collectOddValues(arr) {
+/*function collectOddValues(arr) {
 
     let results = [];
     function helper(helperArr) {
@@ -19,3 +19,42 @@ function collectOddValues(arr) {
 let arr = [1,2,3,4,5,6,7,8,9,10];
 
 console.log(collectOddValues(arr));
+
+// my training 
+function collectOddValues(arr){
+	const odds = [];
+	function helper(helperArr){
+		if(arr.length === 0){
+			return; 
+		}
+		if (arr[0] % 2 !== 0) {
+			odds.push(arr[0]);
+		}
+		helper(arr.splice(0, 1));
+	}
+	helper(arr);
+
+	return odds;
+}*/
+
+function collectOddValuesPureRecursion(arr) {
+	var newArr = [];
+
+	// base
+	if (arr.length === 0)
+		return newArr;
+
+	// pass arr in individual one
+	if (arr[0] % 2 != 0) {
+		newArr.push(arr[0]);
+	}
+
+	newArr = newArr.concat(collectOddValuesPureRecursion(arr.slice(1)));
+	return newArr;
+}
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(collectOddValuesPureRecursion(arr));
+
+
---
+++ recursion/power.js
@@ -0,0 +1,9 @@
+function power (num, exp) {
+	if (exp === 0) {
+		return 1;
+	}
+
+	return num  * power(num, (exp - 1))
+}
+
+console.log(power(2, 4));
