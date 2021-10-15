a99099f3c1a18ff0fe4c3a450fdab4cfc4caa001
Jerome Bidault <djedje.thai.ok@gmail.com>
2021-10-15 18:07:50 +0700

    method pop and shift implemented on singlyLinkedLists

--- dataStructures/singlyLinkedLists/singly_linked_list.js
+++ dataStructures/singlyLinkedLists/singly_linked_list.js
@@ -26,23 +26,55 @@ class SinglyLinkedList {
 		
 		return this
 	}
-	
+
+	// prof solution
 	pop(){
-		const toReturn = this.tail
-		if(this.length < 1) return
-		else {
-			let newSLL = new SinglyLinkedList()
-			let node = this.head
-			for(let i = 0; i < this.length - 1; i++){
-				if(i > 0)node = node.next
-				newSLL.push(node.val)
-			}
-			this.head = newSLL.head
-			this.tail = newSLL.tail
-			this.length = newSLL.length
-			
+		if(!this.head) return undefined
+		var current = this.head
+		var newTail = current
+		while(current.next){
+			newTail = current
+			current = current.next
 		}
-			
+		this.tail = newTail
+		this.tail.next = null
+		this.length--
+		if(this.length === 0){
+			this.head = null
+			this.tail = null
+		}
+
+		return current
+	}
+	
+	// my solution
+	// pop(){
+	// 	if(this.length < 1) return undefined
+	// 	else {
+	// 		const toReturn = this.tail
+	// 		let newSLL = new SinglyLinkedList()
+	// 		let node = this.head
+	// 		for(let i = 0; i < this.length - 1; i++){
+	// 			if(i > 0)node = node.next
+	// 			newSLL.push(node.val)
+	// 		}
+	// 		this.head = newSLL.head
+	// 		this.tail = newSLL.tail
+	// 		this.length = newSLL.length
+	// 		
+	// 		return toReturn
+	// 	}
+	// 		
+	// }
+
+	shift(){
+		if(!this.head) return undefined	
+		let toReturn = this.head
+		this.length--
+		this.head = toReturn.next
+		toReturn.next = null
+		if(this.length < 1) this.tail = null
+
 		return toReturn
 	}
 	
@@ -82,8 +114,13 @@ sll.push(1)
 sll.getTail()
 sll.testNode()
 console.log("=================")
-console.log("res from pop: ", sll.pop())
-// 
+
+console.log(sll.shift())
+
+sll.push(100)
+
+console.log(sll.pop())
+
 sll.getTail()
 sll.testNode()
 
