9edf9a43b20b55b7f6076594f64274ff322b85c0
Jerome Bidault <djedje.thai.ok@gmail.com>
2021-11-16 19:32:19 +0700

    doublyLinkedList done

--- dataStructures/doublyLinkedLists/doubly_linked_list.js
+++ dataStructures/doublyLinkedLists/doubly_linked_list.js
@@ -75,34 +75,74 @@ class DoublyLinkedList {
 	}
 
 	get(index) {
-		if(this.length === 0) return undefined
-		if(index === 0) return this.shift()
-		if(index === this.length - 1) return this.pop()
-
-		let i = 0
-		let node = this.head
-		if(index <= this.length / 2 || index === 0){
-			// start from head
-			while(i <= index){
-				// console.log("node: ", node)
-				if(i === index){
-					let nextNode = node.next
-					let prevNode = node.prev
-					nextNode.prev = prevNode
-					prevNode.next = nextNode
-					return node.val
-				}	
+		if(index < 0 || index >= this.length) return null
+		var i, node
+		if(index <= this.length / 2){
+			i = 0
+			node = this.head
+			while(i !== index){
 				node = node.next
 				i++
 			}
 			
 		} else {
-			// start from tail
-			// implemment here
-			console.log("in else")
+			i = this.length - 1
+			node = this.tail
+			while(index !== i){
+				node = node.prev
+				i--
+			}
 		}
+
+		return node
 	}
 
+	set(index, val) {
+		let indexVal = this.get(index)
+		if(indexVal !== null){
+			indexVal.val = val
+			return true
+		}
+		
+		return false
+	}
+
+
+	insert(index, val) {	
+		if(index === 0) return !!this.unshift(val)
+		if(index === this.length) return !!this.push(val)
+		let indexVal = this.get(index)
+		if(indexVal !== null){
+			let prevIndexVal = indexVal.prev
+			let newNode = new Node(val)
+			indexVal.prev = newNode, newNode.next = indexVal
+			prevIndexVal.next = newNode, newNode.prev = prevIndexVal
+			this.length++
+		
+			return true
+		}
+
+		return false
+	}
+
+	remove(index) {
+		if(index === 0) return this.shift()
+		if(index === this.length -1) return this.pop()
+		let indexVal = this.get(index)
+		if(indexVal !== null){
+			let nextVal = indexVal.next
+			let prevVal = indexVal.prev
+			nextVal.prev = prevVal
+			prevVal.next = nextVal
+
+			indexVal.next = null, indexVal.prev = null
+			this.length--
+			return indexVal
+		}
+		return undefined
+
+	}
+		
 	getHead(){
 		return this.head
 	}
@@ -117,152 +157,10 @@ dl.push(1)
 dl.push(2)
 dl.push(3)
 dl.push(4)
-dl.push(5)
+// dl.push(5)
+console.log("ook: ", dl.remove(-1))
 
-console.log(dl.get(2))
+// console.log(dl.get(0))
 
 console.log("get Head: ", dl.getHead())
 
-
-
-
-
-
-
-// 	push(val){
-// 		var newNode = new Node(val)
-// 		if(!this.head){
-// 			this.head = newNode
-// 			this.tail = newNode
-// 		}
-// 		else {
-// 			this.tail.next = newNode
-// 			this.tail = newNode
-// 		}
-// 		this.length++
-// 		
-// 		return this
-// 	}
-// 
-// 	// prof solution
-// 	pop(){
-// 		if(!this.head) return undefined
-// 		var current = this.head
-// 		var newTail = current
-// 		while(current.next){
-// 			newTail = current
-// 			current = current.next
-// 		}
-// 		this.tail = newTail
-// 		this.tail.next = null
-// 		this.length--
-// 		if(this.length === 0){
-// 			this.head = null
-// 			this.tail = null
-// 		}
-// 
-// 		return current
-// 	}
-// 		
-// 	shift(){
-// 		if(!this.head) return undefined	
-// 		let toReturn = this.head
-// 		this.head = toReturn.next
-// 		toReturn.next = null
-// 		this.length--
-// 		if(this.length < 1) this.tail = null
-// 
-// 		return toReturn
-// 	}
-// 	
-// 	unshift(val){
-// 		var newNode = new Node(val)
-// 		if(!this.head){
-// 			this.head = newNode
-// 			this.tail = newNode
-// 		}
-// 		else {
-// 			newNode.next = this.head
-// 			this.head = newNode
-// 		}
-// 		this.length++
-// 		
-// 		return this
-// 	}
-// 
-// 	// here i replace the value of the node at the index arg
-// 	set(index, value){
-// 		let node = this.get(index)
-// 		if(node) {
-// 			node.val = value
-// 			return true
-// 		}
-// 
-// 		return false
-// 	}
-// 
-// 	get(index) {
-// 		if(this.length <= index || index < 0) return undefined
-// 		let count = 0
-// 		let current = this.head
-// 		while(count != index){
-// 			current = current.next
-// 			count++
-// 		}
-// 
-// 		return current
-// 	}
-// 
-// 	insert(index, value) {
-// 		// prof
-// 		if(index > this.length || index < 0) return false
-// 		if(index === this.length) return !!this.push(value)
-// 		if(index === 0) return !!this.unshift(value)
-// 		let nodeRef = this.get(index - 1)
-// 		let newNode = new Node(value)
-// 		newNode.next = nodeRef.next
-// 		nodeRef.next = newNode
-// 
-// 		this.length++
-// 
-// 		return true
-// 	}
-// 
-// 	remove(index){
-// 		if(index > this.length -1 || index < 0) return undefined
-// 		if(index === 0) return this.shift(index)
-// 		if(index === this.length - 1)return this.pop(index)
-// 		let nodeRemoved = this.get(index) 
-// 		let previousIndex = this.get(index - 1)
-// 		previousIndex.next = previousIndex.next.next
-// 		this.length--
-// 
-// 		return nodeRemoved
-// 	}
-// 	
-// 	reverse(){
-// 		// let newNode = new Node(this.head.val)
-// 		let node = this.head
-// 		this.head = this.tail
-// 		this.tail = node
-// 		let next
-// 		let prev = null
-// 		for(let i = 0; i < this.length; i++){
-// 			next = node.next
-// 			node.next = prev
-// 			prev = node
-// 			node = next
-// 		}
-// 		
-// 		return this
-// 	}
-// 	
-// 	getTail(){
-// 		console.log(this.tail, " - ", this.length)	
-// 	}
-// 
-// 	testNode(){
-// 		console.log(this.head)
-// 	}
-// }
-// 
